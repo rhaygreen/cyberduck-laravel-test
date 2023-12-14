93ee44c191bb70b7723e9fd4de243a365c4a79ab
Richard Haygreen <richard.haygreen@idgateway.co.uk>
2023-12-14 15:29:17 +0000

    Changed name of DTO to better suit its actual usage

---
+++ app/DTOs/ProductQuantityPriceDTO.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DTOs;
+
+use App\Models\Product;
+use Akaunting\Money\Money;
+
+class ProductQuantityPriceDTO
+{
+    public function __construct(
+        private Product $product,
+        private int $quantity,
+        private Money $unitPrice,
+        ) {}
+
+    public function getProduct()
+    {
+        return $this->product;
+    }
+
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+
+    public function getUnitPrice()
+    {
+        return $this->unitPrice;
+    }
+}
