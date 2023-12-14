26ac22a645d142e132fa5d809b91b6f957936849
Richard Haygreen <richard.haygreen@idgateway.co.uk>
2023-12-14 16:44:33 +0000

    Adds new option for disabling for submission button

--- resources/js/components/newCoffeeSale.vue
+++ resources/js/components/newCoffeeSale.vue
@@ -8,33 +8,49 @@ export default {
             product_id: 1,
             quantity: '',
             unit_cost: '',
-            selling_price: ''
-        }
+            selling_price: '',
+            button_disabled: false
+        },
+        button: {
+            text: 'Record Sale'
+        },
     };
   },
   methods: {
     async loadPrice() {
         this.data.selling_price = '';
         if(!isNaN(parseFloat(this.data.quantity)) && !isNaN(parseFloat(this.data.unit_cost))) {
-            const url = '/product/'+this.data.product_id+'/calculateSellingPrice?quantity='+this.data.quantity+'&unit_price='+this.data.unit_cost
+            const url = '/product/'+this.data.product_id+'/calculateSellingPrice?quantity='+this.data.quantity+'&unit_cost='+this.data.unit_cost
             await axios.get(url)
             .then((response) => this.data.selling_price = response.data )
             .catch(function (error){
                 alert('Sorry, something went wrong. Please try again');
-                console.log(error.toJSON());
+                console.log(error);
             });
         }
     },
     async recordSale() {
         if(!isNaN(parseFloat(this.data.quantity)) && !isNaN(parseFloat(this.data.unit_cost))) {
             //add debounce to the button. Remove once request returns.
+            this.data.button_disabled = true;
+            this.button.text = 'Recording...';
 
             const url = '/sale/store';
-            await axios.post(url)
-            .then((response) => this.data.selling_price = response.data )
-            .catch(function (error){
+            await axios.post(url,{
+                id_product: this.data.product_id,
+                quantity: this.data.quantity,
+                unit_cost: this.data.unit_cost
+
+            })
+            .then((response) => {
+                this.data.button_disabled = false;
+                this.button.text = 'Record Sale';
+            })
+            .catch((error) => {
                 alert('Sorry, something went wrong. Please try again');
-                console.log(error.toJSON());
+                console.log(error);
+                this.data.button_disabled = false;
+                this.button.text = 'Record Sale';
             });
         }
         else {
@@ -87,8 +103,9 @@ export default {
             :columns="{ container: 3, label: 12, wrapper: 12 }"
             name="record_sale"
             id="record_sale"
+            :disabled="button_disabled"
             @click="recordSale"
-            >Record Sale
+            >{{ button.text }}
         </ButtonElement>
         <StaticElement
             :columns="12"
