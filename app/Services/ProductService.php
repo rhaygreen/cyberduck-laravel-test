5f0df243ae004da41b28cecd225140556bbe8905
Richard Haygreen <richard.haygreen@idgateway.co.uk>
2023-12-14 20:23:18 +0000

    Adds a method for correctly rounding a currency value; rounds up the decimal values to 2 dp

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -20,6 +20,23 @@ class ProductService
         $cost = $dto->getQuantity() * $dto->getUnitPrice()->getAmount();
         $sellingPrice = ($cost / (1 - $formattedMargin)) + $product->shipping_cost->getAmount();
 
-        return new Money($sellingPrice, new Currency('GBP'));
+        return new Money($this->formatDecimalInCurrency($sellingPrice), new Currency('GBP'));
+    }
+
+    /**
+     * Handles a currency amount where there is more than 2 decimal places, since the
+     * amount must be rounded up in this case.
+     */
+    private function formatDecimalInCurrency(float $needsFormatting): float
+    {
+        $numbers = explode('.', $needsFormatting);
+        if(empty($numbers[1]) || count_chars($numbers[1]) < 3) {
+            return $needsFormatting;
+        }
+
+        //if the number has a decimal precision of 3 or more, then we need to truncate it and increase the last digit by one.
+        $toKeep = substr($numbers[1],0,2);
+        return (float) $numbers[0] . '.' . (++$toKeep);
+
     }
 }
